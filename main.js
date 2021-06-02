21662b2903601d15526d26bbaaaf83c0d6d35f27
Jemma <jjsplodge@hotmail.com>
2021-06-02 14:28:05 +0100

    commiting new things

---
+++ main.js
@@ -0,0 +1,7 @@
+const splash = document.querySelector('.opening-hexs');
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  setTimeout(() => {
+    splash.classList.add('display-none');
+  }, 2000);
+})
